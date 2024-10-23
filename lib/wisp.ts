b5a69209954e37b884ffaefe20f0f436b9513788
glassesmonkey <530515490@qq.com>
2024-10-23 15:19:26 +0800

    change the blog id

--- lib/wisp.ts
+++ lib/wisp.ts
@@ -7,7 +7,7 @@ async function getWispClient() {
     const { buildWispClient } = await import('@wisp-cms/client');
     wispClient = buildWispClient({
       baseUrl: "https://www.wisp.blog",
-      blogId: "cm21qzfj5000012w8pe1wjbml", // 请替换为您的实际 Blog ID
+      blogId: "cm2ljg81f000037ldxfd4occp", // 请替换为您的实际 Blog ID
     });
   }
   return wispClient;
